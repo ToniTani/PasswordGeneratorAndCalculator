e338fb74d57f552945eb87c5e6be8acfbeeb7b8e
ToniTani <toni.tani.tt@gmail.com>
2022-06-11 02:45:37 +0300

    template for generator app

--- calculator_password/generator/views.py
+++ calculator_password/generator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html', {'password':'randwee32qjj2'})
+
+def eggs(request):
+    return HttpResponse ('<h1> eggs! </h1>')
