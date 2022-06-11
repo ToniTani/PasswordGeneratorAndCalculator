c33daa31d352a9eabcf08101ecb6954836528eaa
ToniTani <toni.tani.tt@gmail.com>
2022-06-12 02:21:10 +0300

    Created password generator logic and about page.

--- calculator_password/generator/views.py
+++ calculator_password/generator/views.py
@@ -6,14 +6,24 @@ import random
 def home(request):
     return render(request, 'generator/home.html')
 
+def about(request):
+    return render(request, 'generator/about.html')
+
 def password(request):
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('@£$€&/()=!#¤%'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
 
     thepassword = ''
     for x in range(length):
-        thepassword += random.choice.(characters)
+        thepassword += random.choice(characters)
 
-    return HttpResponse (request, 'generator/password.html', {'password':thepassword})
+    return render (request, 'generator/password.html', {'password':thepassword})
